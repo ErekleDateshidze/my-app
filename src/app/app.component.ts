3aa050ac491639602a959823343f091cd565fe8a
Erekle99 <erekledateshidze99@gmail.com>
2023-01-17 00:31:15 +0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,59 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+// interface inputSelect {
+//   taskName:string;
+//   selectValue:string;
+// }
+
+// interface lists {
+//   state:string;
+ 
+// }
+
+
 export class AppComponent {
-  title = 'my-app';
+  tasks:string[]=[];
+  taskName :string=''
+  error=''
+  options = [ ' Easy' , 'Medium' , 'Hard'];
+  selectedValue:string='';
+
+  
+
+
+
+  addTask() {
+    if(!this.taskName) {
+      this.error= 'Empty Input'
+      return
+    }
+    this.tasks.push(this.taskName);
+    this.taskName='';
+    this.error=''
+  }
+
+
+  getItemClass (index :number) {
+    return {
+      red:index %2 ==0,
+      blue: index %2==1 ,
+    };
+  }
+
+  showNumber(index : number) {
+    return index;
+
+  }
+
+  deleteItem(index:number){
+    // this.tasks= this.tasks.filter((_,i) => i !==index)
+    this.tasks.splice(index,1);
+  }
+
+  moveToProgress(index :number) {
+    
+  }
+
 }
